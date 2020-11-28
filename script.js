cee7d6f7ff954159de95aac2b5b3980fbf413241
Titan Chen <titanchen@yahoo.com>
2020-11-28 18:30:54 -0500

    implemented color change option

--- script.js
+++ script.js
@@ -20,18 +20,29 @@ function draw(color){
     let squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
         square.addEventListener('mouseover', () => {
-            square.classList.add(color);
+            square.setAttribute('class', '');
+            square.classList.add('square');
+            if(color=='black'){
+                square.style.backgroundColor = 'grey';
+                square.classList.add('black');
+            } else {
+                console.log(Math.floor(Math.random*255));
+                square.style.backgroundColor = 'rgb(' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ')';
+                square.classList.add('rainbow');
+            }
         })
     });
 }
 
+//randomly generates
 
 //main
 
 //create default 4x4 grid
 drawGrid(16);
-draw("drawn-black");
+draw("black");
 
+//EventListener for left knob to reset the grid.
 let reset = document.querySelector('#left-knob');
 reset.addEventListener('click', () =>{
     let size = prompt('How many squares per side?');
@@ -39,26 +50,29 @@ reset.addEventListener('click', () =>{
         size = prompt('Size cannot be greater than 100 or less than 0.'
         + ' please enter another size');
     }
-    let knob = document.querySelector('#right-knob');
-    knob.addEventListener('click', () => {
-        let color = knob.getAttribute('data-color');
-        if(color=='black'){
-            knob.textContent = '';
-            knob.appendChild(document.textContent('black'));
-            knob.setAttribute('data-color', 'rainbow');
-            knob.classList.remove('drawn-black');
-            knob.classList.add('drawn-rainbow');
-            draw('drawn-rainbow');
-        } else {
-            knob.textContent = '';
-            knob.appendChild(document.textContent('rainbow'));
-            knob.setAttribute('data-color', 'black');
-            draw('drawn-black');
-            knob.classList.remove('drawn-rainbow');
-            knob.classList.add('drawn-black');
-        }
-    })
-    
     drawGrid(size*size);
-    draw("drawn-black");
+    draw("black");
+    let knob = document.getElementById('right-knob');
+    knob.textContent = 'rainbow';
+})
+
+//EventListener for right knob to alternate between rainbow and black
+let knob = document.querySelector('#right-knob');
+knob.addEventListener('click', () => {
+    let color = knob.getAttribute('data-color');
+    if(color=='black'){
+        knob.textContent = '';
+        knob.appendChild(document.createTextNode('black'));
+        knob.setAttribute('data-color', 'rainbow');
+        knob.classList.remove('black');
+        knob.classList.add('rainbow');
+        draw('rainbow');
+    } else {
+        knob.textContent = '';
+        knob.appendChild(document.createTextNode('rainbow'));
+        knob.setAttribute('data-color', 'black');
+        knob.classList.remove('rainbow');
+        knob.classList.add('black');
+        draw('black');
+    }
 })
